444c56e2dcf1b780ebfdc62345b6b2ea840ed159
mkavcar <mkavcar@gmail.com>
2016-05-27 10:44:02 -0400

    Add project files.

---
+++ src/WebApiTest/Controllers/NytProxyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class NytProxyController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public async Task<dynamic> Get(string section)
+        {
+            if (string.IsNullOrEmpty(section)) section = "world";
+            var url = $"http://api.nytimes.com/svc/topstories/v1/{section}.json?api-key=70444bc7247c7b99f726453419c61740:5:74385476";
+            
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync(url);
+                return result.Content.ReadAsStringAsync().Result;
+            }
+        }
+    }
+}
